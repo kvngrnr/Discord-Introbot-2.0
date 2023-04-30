3ab01d4eab64a9c5bcb176dd6c78edef100a6f9e
Kevin <kevin.greiner1999@gmail.com>
2023-05-01 00:48:59 +0200

    initial commit

--- src/main/java/com/jagrosh/jmusicbot/commands/music/LyricsCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/music/LyricsCmd.java
@@ -20,6 +20,7 @@ import com.jagrosh.jlyrics.LyricsClient;
 import com.jagrosh.jmusicbot.Bot;
 import com.jagrosh.jmusicbot.audio.AudioHandler;
 import com.jagrosh.jmusicbot.commands.MusicCommand;
+
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 
